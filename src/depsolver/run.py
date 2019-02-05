8d0cf3dfee9cfe62a89f1d8c1925e694fe174891
Edward J Brown <edward.brown.96@live.co.uk>
2019-02-05 22:47:19 +0000

    Update run.py

--- src/depsolver/run.py
+++ src/depsolver/run.py
@@ -1,5 +1,19 @@
-from z3 import Int, Bool, solve
+from parse import parse_repository, parse_final_state_constraints
+from repository import repository_to_dictionary
+from solver import make_sat_problem
 
-x = Int('x')
-y = Int('y')
-solve(x > 2, y < 10, x + 2*y == 7)
+package_list = parse_repository("tests/example-0/repository.json")
+package_dict = repository_to_dictionary(package_list)
+final_state = parse_final_state_constraints("tests/example-0/constraints.json")
+
+step_limit = 2
+assert step_limit >= 2
+sat_problem = \
+    make_sat_problem(package_list, package_dict, final_state, step_limit)
+
+print(sat_problem)
+
+
+# x = Int('x')
+# y = Int('y')
+# solve(x > 2, y < 10, x + 2*y == 7)
