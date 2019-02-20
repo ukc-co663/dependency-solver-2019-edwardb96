455c9f7b287465da7b1c751a8f3b4cafa46980a2
Edward J Brown <edward.brown.96@live.co.uk>
2019-02-20 16:40:05 +0000

    Added symlink so rust can do its thing

--- install_deps.sh
+++ install_deps.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 apt-get update
 apt-get install -y libz3-4 cargo #python3-pip
+ln -s /usr/lib/x86_64-linux-gnu/libz3.so.4 /usr/lib/x86_64-linux-gnu/libz3.so
 #pip3 install --user z3-solver
