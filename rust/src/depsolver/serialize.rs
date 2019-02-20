7be6890c51d1af56f89095d18ee677a4d4987170
Edward J Brown <edward.brown.96@live.co.uk>
2019-02-20 17:41:52 +0000

    Used println instead of writing to stdout
    Getting desparate now.

--- rust/src/depsolver/serialize.rs
+++ rust/src/depsolver/serialize.rs
@@ -2,6 +2,7 @@ use crate::depsolver::model::command::Command;
 use std::io::{stdout, Write};
 
 pub fn serialize_commands(commands: &Vec<Command>) {
-    serde_json::to_writer(stdout(), commands).expect("Error serializing command.");
-    stdout().write(b"\n").expect("Failed to write a newline, how pathetic");
+ //   serde_json::to_writer(stdout(), commands).expect("Error serializing command.");
+  //  stdout().write(b"\n").expect("Failed to write a newline, how pathetic");
+  println!("{}", &serde_json::to_string(commands).expect("Failed to serialze commands"));
 }
