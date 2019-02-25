7e8e9a61abd95db2ca48c4828845f3afa05ccc9a
Edward J Brown <edward.brown.96@live.co.uk>
2019-02-25 15:43:00 +0000

    Switch to prebuilt z3

--- install_deps.sh
+++ install_deps.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 apt-get update
-apt-get install -y libz3-4 cargo #python3-pip
-ln -s /usr/lib/x86_64-linux-gnu/libz3.so.4 /usr/lib/x86_64-linux-gnu/libz3.so
+apt-get install -y libz3-4 cargo
+cp dependencies/libz3.so /usr/lib/x86_64-linux-gnu/libz3.so
 #pip3 install --user z3-solver
