6a28f3a9830e449f35228ff8c04dfe3ffc425247
Edward J Brown <edward.brown.96@live.co.uk>
2019-03-08 10:12:13 +0000

    Test score with python version

--- compile.sh
+++ compile.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cd rust
-cargo build --release
+#cd rust
+#cargo build --release
--- install_deps.sh
+++ install_deps.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-apt-get update
-apt-get install -y libz3-4 cargo
-cp dependencies/libz3.so /usr/lib/x86_64-linux-gnu/libz3.so
-#pip3 install --user z3-solver
+#apt-get update
+apt-get install -y python3 python3-pip z3 #libz3-4 cargo
+#cp dependencies/libz3.so /usr/lib/x86_64-linux-gnu/libz3.so
+pip3 install --user z3-solver
